32544bef9fdf1ea07f9001e4373ac0c68fac59d1
vinichagastrexx <vinicius@trexx.com.br>
2023-06-02 11:35:21 -0300

    style: customize home page with bbg setup

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,33 +1,24 @@
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import { ConnectWallet } from "@thirdweb-dev/react";
 import NextLink from "next/link";
-import { Avatar, Link, Text, Box, Flex, Heading } from "@chakra-ui/react";
+import { Link, Text, Box, Flex, Heading, Image } from "@chakra-ui/react";
 
 export function Navbar() {
-  const address = useAddress();
-
   return (
-    <Box maxW={"1200px"} m={"auto"} py={"10px"} px={"40px"}>
+    <Box maxW={"1280px"} m={"auto"} py={"10px"} px={"40px"}>
       <Flex justifyContent={"space-between"} alignItems={"center"}>
         <Link as={NextLink} href={'/'}>
-          <Heading>
-            NFTRentMarketplace
-          </Heading>
+          <Image src="/logo_bbg.png" alt="Logo" boxSize="100px" mr="20px" width={"100%"} />
         </Link>
         <Flex direction={"row"}>
-          <Link as={NextLink} href={'/rent'} mx={2.5}>
-            <Text>Rent</Text>
+          <Link as={NextLink} href={'/pools'} mx={2.5} fontFamily={"Bayon"} fontSize={"30"}>
+            <Text>Pools</Text>
           </Link>
-          <Link as={NextLink} href={'/add-to-pool'} mx={2.5}>
-            <Text>Add Item To Pool</Text>
+          <Link as={NextLink} href={'/inventory'} mx={2.5} fontFamily={"Bayon"} fontSize={"30"}>
+            <Text>Inventory</Text>
           </Link>
         </Flex>
         <Flex direction={"row"} alignItems={"center"}>
           <ConnectWallet />
-          {address && (
-            <Link as={NextLink} href={`/profile/${address}`}>
-              <Avatar src='https://bit.ly/broken-link' ml={"20px"} />
-            </Link>
-          )}
         </Flex>
       </Flex>
     </Box>
--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -2,7 +2,7 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import { ChakraProvider } from '@chakra-ui/react';
 import { Navbar } from "../components/Navbar";
-
+import Head from 'next/head'
 
 const activeChain = "mumbai";
 
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <ChakraProvider>
+        <Head>
+          <title>BBGMarketplace</title>
+          <meta property="og:title" content="BBGMarketplace" key="title" />
+          <link rel="icon" href="/icons/favicon.ico" />
+        </Head>
         <Navbar />
         <Component {...pageProps} />
       </ChakraProvider>
