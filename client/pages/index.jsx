89cb07ec6f053a53ce339836222ad3be781e3d66
vinichagastrexx <vinicius@trexx.com.br>
2023-06-02 11:35:40 -0300

    feat: reorganize pages

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,21 +1,29 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import { Button, Container, Flex, Heading, Stack } from "@chakra-ui/react"
+import { Box, Button, Container, Flex, Stack } from "@chakra-ui/react"
 import NextLink from 'next/link'
 
 export default function Home() {
   return (
-    <Container maxW={"1200px"}>
-      <Flex h={"80vh"} alignItems={"center"} justifyContent={"center"}>
-        <Stack spacing={4} align={"center"}>
-          <Heading>Marketplace</Heading>
-          <Button as={NextLink} href='/rent'>
-            Rent NFTs
-          </Button>
-          <Button as={NextLink} href='/inventory'>
-            Check your game inventory
-          </Button>
-        </Stack>
-      </Flex>
+    <Container maxW={"100%"}>
+      <Box
+        h={"80vh"}
+        w={"100%"}
+        backgroundImage="url('/bbg_concept.jpg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+      >
+        <Flex h={"100%"} alignItems={"center"} justifyContent={"center"}>
+          <Stack spacing={4} align={"center"}>
+            <Button as={NextLink} href='/pools'>
+              Rent incredible Items
+            </Button>
+            <Button as={NextLink} href='/inventory'>
+              Check your game inventory
+            </Button>
+          </Stack>
+        </Flex>
+      </Box>
     </Container>
   );
 }
