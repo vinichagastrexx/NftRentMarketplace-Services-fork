40c2d824fb0a07575cc893d6007da05035beaee5
vinichagastrexx <vinicius@trexx.com.br>
2023-06-03 10:50:57 -0300

    refactor: remove unused vars

--- client/components/NFTCard.jsx
+++ client/components/NFTCard.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { NFT } from '@thirdweb-dev/sdk';
-import { NFT_RENT_MARKETPLACE_ADDRESS, NFT_ADDRESS } from '../const/addresses'
-import { ThirdwebNftMedia, useContract, useNFT } from '@thirdweb-dev/react';
-import { Box, Flex, Skeleton, Text } from '@chakra-ui/react';
+import { ThirdwebNftMedia } from '@thirdweb-dev/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 
 export default function NFTCard({ nft }) {
-  const { contract, isContractLoading } = useContract(NFT_ADDRESS, "nft-collection");
-
   return (
     <Flex direction={"column"} justifyContent={"center"}>
       <Box overflow={"hidden"}>
