a56f15ae4ae1f712f31e7c7d4e97f905e11eb504
vinichagastrexx <vinicius@trexx.com.br>
2023-06-04 10:30:16 -0300

    style: update Navbar style and add chakra/icons

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,16 +7,16 @@ export function Navbar() {
     <Box maxW={"1280px"} m={"auto"} py={"10px"} px={"40px"}>
       <Flex justifyContent={"space-between"} alignItems={"center"}>
         <Link as={NextLink} href={'/'}>
-          <Image src="/logo_bbg.png" alt="Logo" boxSize="100px" mr="20px" width={"100%"} />
+          <Image objectFit="contain" src="/logo_bbg.png" obj alt="Logo" boxSize="100px" mr="20px" width={"100%"} />
         </Link>
         <Flex direction={"row"}>
-          <Link as={NextLink} href={'/pools'} mx={2.5} fontFamily={"Bayon"} fontSize={"30"}>
+          <Link as={NextLink} href={'/pools'} mx={2.5} fontFamily={"Bayon"} fontSize={"22"}>
             <Text>Pools</Text>
           </Link>
-          <Link as={NextLink} href={'/inventory'} mx={2.5} fontFamily={"Bayon"} fontSize={"30"}>
+          <Link as={NextLink} href={'/inventory'} mx={2.5} fontFamily={"Bayon"} fontSize={"22"}>
             <Text>Inventory</Text>
           </Link>
-          <Link as={NextLink} href={'https://boomboogers.com.br/'} mx={2.5} fontFamily={"Bayon"} fontSize={"30"}>
+          <Link as={NextLink} href={'https://boomboogers.com.br/'} mx={2.5} fontFamily={"Bayon"} fontSize={"22"}>
             <Text>Know the game!</Text>
           </Link>
         </Flex>
