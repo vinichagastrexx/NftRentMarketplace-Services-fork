f682a2b84abbcc29a03ea5f285aa8328d74bf01c
vinichagastrexx <vinicius@trexx.com.br>
2023-06-05 09:58:33 -0300

    style: update style of NFT and Pool Cards

--- client/components/NFT/NFTCard.jsx
+++ client/components/NFT/NFTCard.jsx
@@ -4,14 +4,37 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 
 export default function NFTCard({ nft }) {
   return (
-    <Flex overflow={"hidden"} borderRadius={'10px'} border={'1px'} direction={"column"} maxWidth={200} justifyContent={"center"}>
+    <Flex
+      overflow={"hidden"}
+      borderRadius={'10px'}
+      border={'1px'}
+      direction={"column"}
+      maxWidth={200}
+      justifyContent={"center"}
+      transition="0.3s"
+      _hover={{ transform: 'scale(1.05)' }}
+    >
       <Box >
-        <ThirdwebNftMedia width='200px' height='150px' style={{}}
+        <ThirdwebNftMedia
+          width='200px'
+          height='150px'
+          style={{}}
           metadata={nft.metadata} />
       </Box>
-      <Box background="linear-gradient(to right, #ffafbd, #ffc3a0)" p={4}>
-        <Text>Token Id: {nft.metadata.id}</Text>
-        <Text>Item name: {nft.metadata.name}</Text>
+      <Box
+        background="linear-gradient(to right, #FFFFFF, #48E3C8)"
+        p={4}
+      >
+        <Text
+          fontSize="m"
+          fontWeight="bold"
+          fontFamily={"Bayon"}
+          mb={1}
+        >Token Id: {nft.metadata.id}</Text>
+        <Text
+          fontSize="m"
+          fontFamily={"big shoulders text"}
+        >Item name: {nft.metadata.name}</Text>
       </Box>
     </Flex>
   )
--- client/components/Pool/PoolCard.jsx
+++ client/components/Pool/PoolCard.jsx
@@ -3,14 +3,37 @@ import { Image, Box, Flex, Text, Divider } from '@chakra-ui/react';
 
 export default function PoolCard({ pool }) {
   return (
-    <Flex overflow={"hidden"} borderRadius={'10px'} border={'1px'} direction={"column"} maxWidth={200} justifyContent={"center"}>
+    <Flex
+      overflow={"hidden"}
+      borderRadius={'10px'}
+      border={'1px'}
+      direction={"column"}
+      maxWidth={200}
+      justifyContent={"center"}
+    >
       <Box>
         <Image src={pool.IMAGEURL} alt={pool.ID} width={"100%"} objectFit="cover" />
       </Box>
-      <Box background="linear-gradient(to right, #ffafbd, #ffc3a0)" p={4}>
-        <Text fontSize={18} fontFamily={"Big Shoulders Text"}>{pool.CATEGORYTYPE} Pool</Text>
-        <Text fontSize={18} fontFamily={"Big Shoulders Text"}>Base Price: {pool.BASEPRICE}</Text>
-        <Text fontSize={18} fontFamily={"Big Shoulders Text"}>Rarity: {pool.RARITY}</Text>
+      <Box
+        background="linear-gradient(to right, #FFFFFF, #48E3C8)"
+        p={4}
+      >
+        <Text
+          fontSize="m"
+          fontWeight="bold"
+          fontFamily={"Bayon"}
+          mb={1}
+        >{pool.CATEGORYTYPE} Pool</Text>
+        <Text
+          fontSize="m"
+          fontFamily={"big shoulders text"}
+          mb={1}
+        >Base Price: {pool.BASEPRICE}</Text>
+        <Text
+          fontSize="m"
+          fontFamily={"big shoulders text"}
+          mb={1}
+        >Rarity: {pool.RARITY}</Text>
       </Box>
     </Flex >
   )
