79f95acdb40c221fa53758f1da7e2f5928665f79
vinichagastrexx <vinicius@trexx.com.br>
2023-06-05 07:29:18 -0300

    refactor: remove unused code NFTGrid

--- client/components/NFT/NFTGrid.jsx
+++ client/components/NFT/NFTGrid.jsx
@@ -21,14 +21,12 @@ export default function NFTGrid({
           [...Array(3)].map((_, index) => (
             <Skeleton key={index} height={"150px"} width={"250px"} />
           ))
-        ) : data && data.length > 0 ? (
+        ) : data && data.length > 0(
           data.map((nft) => (
             <div key={nft.metadata.id} onClick={() => handleNFTClick(nft)}>
               <NFTCard nft={nft}></NFTCard>
             </div>
           ))
-        ) : (
-          <Text fontSize={25} fontFamily={"Big Shoulders Text"}>You don't have any NFTs, what about Renting from a pool?</Text>
         )}
       </SimpleGrid>
       <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="xs">
