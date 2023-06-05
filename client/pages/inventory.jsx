e651ed4d9dba0c5e54b73a02b8434947e84eacc6
vinichagastrexx <vinicius@trexx.com.br>
2023-06-05 15:54:23 -0300

    style: add Accordion in inventory and profile

--- client/pages/inventory.jsx
+++ client/pages/inventory.jsx
@@ -12,6 +12,12 @@ import {
   SimpleGrid,
   Skeleton,
   Button,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
 } from '@chakra-ui/react';
 import {
   useContract,
@@ -63,70 +69,106 @@ export default function Inventory() {
   );
   return (
     <Container maxW={'75%'} p={5}>
-      <Heading fontSize={40} fontFamily={'Bayon'}>
-        Your Owned Items
-      </Heading>
-      {isLoading ? (
-        <></>
-      ) : ownedNfts?.length > 0 ? (
-        <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
-          These are the game items you own. Put them in a pool and start generating income!
-        </Text>
-      ) : (
-        <div>
-          <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
-            You don't have any Items! Check the available Pools and rent some
-            items to play!
-          </Text>
-          <Button
-            letterSpacing={0.5}
-            marginTop={5}
-            size={'lg'}
-            as={NextLink}
-            colorScheme="teal"
-            href="/pools"
-            color={'white'}
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton
+            _hover={{
+              bg: '#FBAA0B',
+              transition: 'background-color 0.2s',
+              color: 'white'
+            }}
+            padding={10}
           >
-            Rent incredible Items
-          </Button>
-        </div>
-      )}
-      <NFTGrid isLoading={isLoading} data={ownedNfts} />
-      <Heading fontSize={40} fontFamily={'Bayon'}>
-        Your Rented Items
-      </Heading>
-      {rentedItems?.rents.length > 0 ? (
-        <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
-          These are the items you are currently renting from other players for use in the game.
-        </Text>
-      ) : (
-        <></>
-      )}
-      <SimpleGrid
-        minChildWidth="200px"
-        spacing={2}
-        maxW={'100%'}
-        padding={2}
-        my={4}
-      >
-        {rentedItemsLoading ? (
-          [...Array(3)].map((_, index) => (
-            <Skeleton key={index} height={'150px'} width={'250px'} />
-          ))
-        ) : rentedItems?.rents.length > 0 ? (
-          rentedItems?.rents?.map((rentedItem) => (
-            <RentedNFT
-              key={rentedItem.NFTID}
-              nftId={rentedItem.NFTID}
-              rentData={rentedItem}
-            />
-          ))
-        ) : (
-          <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
-            0 items found
-          </Text>
-        )}
-      </SimpleGrid>
+            <Box flex="1" textAlign="left">
+              <Heading fontSize={40} fontFamily={'Bayon'}>
+                Your Owned Items
+              </Heading>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel paddingLeft={10} pb={4}>
+            {isLoading ? (
+              <></>
+            ) : ownedNfts?.length > 0 ? (
+              <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
+                These are the game items you own. Put them in a pool and start
+                generating income!
+              </Text>
+            ) : (
+              <div>
+                <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
+                  You don't have any Items! Check the available Pools and rent
+                  some items to play!
+                </Text>
+                <Button
+                  letterSpacing={0.5}
+                  marginTop={5}
+                  size={'lg'}
+                  as={NextLink}
+                  colorScheme="teal"
+                  href="/pools"
+                  color={'white'}
+                >
+                  Rent incredible Items
+                </Button>
+              </div>
+            )}
+            <NFTGrid isLoading={isLoading} data={ownedNfts} />
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton
+            _hover={{
+              bg: '#FBAA0B',
+              transition: 'background-color 0.2s',
+              color: 'white'
+            }}
+            padding={10}
+          >
+            <Box flex="1" textAlign="left">
+              <Heading fontSize={40} fontFamily={'Bayon'}>
+                Your Rented Items
+              </Heading>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel paddingLeft={10} pb={4}>
+            {rentedItems?.rents.length > 0 ? (
+              <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
+                These are the items you are currently renting from other players
+                for use in the game.
+              </Text>
+            ) : (
+              <></>
+            )}
+            <SimpleGrid
+              minChildWidth="200px"
+              spacing={2}
+              maxW={'100%'}
+              padding={2}
+              my={4}
+            >
+              {rentedItemsLoading ? (
+                [...Array(3)].map((_, index) => (
+                  <Skeleton key={index} height={'150px'} width={'250px'} />
+                ))
+              ) : rentedItems?.rents.length > 0 ? (
+                rentedItems?.rents?.map((rentedItem) => (
+                  <RentedNFT
+                    key={rentedItem.NFTID}
+                    nftId={rentedItem.NFTID}
+                    rentData={rentedItem}
+                  />
+                ))
+              ) : (
+                <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
+                  0 items found
+                </Text>
+              )}
+            </SimpleGrid>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Container>
   );
 }
--- client/pages/profile/[address].jsx
+++ client/pages/profile/[address].jsx
@@ -3,24 +3,27 @@ import {
   Heading,
   Text,
   SimpleGrid,
+  Box,
   Skeleton,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 import { useContract, useNFT, useAddress } from '@thirdweb-dev/react';
 import React from 'react';
-import {
-  NFT_ADDRESS,
-} from '../../const/addresses';
+import { NFT_ADDRESS } from '../../const/addresses';
 import useSWR from 'swr';
 import NFTCard from '../../components/NFT/NFTCard';
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function RentedNFT({ nftId }) {
   const { contract: nftCollection } = useContract(NFT_ADDRESS);
   const { data: nft } = useNFT(nftCollection, nftId);
 
-  return nft ? (
-    <NFTCard key={nft.metadata.id} nft={nft} />
-  ) : null;
+  return nft ? <NFTCard key={nft.metadata.id} nft={nft} /> : null;
 }
 
 export default function ProfilePage() {
@@ -38,60 +41,97 @@ export default function ProfilePage() {
 
   return (
     <Container maxW={'75%'} p={5}>
-      <Heading fontSize={40} fontFamily={'Bayon'}>
-        Your Rented Out Items
-      </Heading>
-      <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
-        Items that you own and are currently rented by other players.
-      </Text>
-      <SimpleGrid
-        minChildWidth="200px"
-        spacing={2}
-        maxW={'100%'}
-        padding={2}
-        my={4}
-      >
-        {rentedItemsLoading ? (
-          [...Array(3)].map((_, index) => (
-            <Skeleton key={index} height={'150px'} width={'250px'} />
-          ))
-        ) : rentedItems?.itemsRented?.length > 0 ? (
-          rentedItems?.itemsRented?.map((rentedItem) => (
-            <RentedNFT key={rentedItem.NFTID} nftId={rentedItem.NFTID} />
-          ))
-        ) : (
-          <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
-            0 items found
-          </Text>
-        )}
-      </SimpleGrid>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton
+            _hover={{
+              bg: '#FBAA0B',
+              transition: 'background-color 0.2s',
+              color: 'white'
+            }}
+            colorScheme="teal"
+            padding={10}
+          >
+            <Box flex="1" textAlign="left">
+              <Heading fontSize={40} fontFamily={'Bayon'}>
+                Your Rented Out Items
+              </Heading>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel paddingLeft={10} pb={4}>
+            <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
+              Items that you own and are currently rented by other players.
+            </Text>
+            <SimpleGrid
+              minChildWidth="200px"
+              spacing={2}
+              maxW={'100%'}
+              padding={2}
+              my={4}
+            >
+              {rentedItemsLoading ? (
+                [...Array(3)].map((_, index) => (
+                  <Skeleton key={index} height={'150px'} width={'250px'} />
+                ))
+              ) : rentedItems?.itemsRented?.length > 0 ? (
+                rentedItems?.itemsRented?.map((rentedItem) => (
+                  <RentedNFT key={rentedItem.NFTID} nftId={rentedItem.NFTID} />
+                ))
+              ) : (
+                <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
+                  0 items found
+                </Text>
+              )}
+            </SimpleGrid>
+          </AccordionPanel>
+        </AccordionItem>
 
-      <Heading fontSize={40} fontFamily={'Bayon'}>
-        Your Items in Pools
-      </Heading>  <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
-        These are the items you've placed in pools, available for others to rent
-      </Text>
-      <SimpleGrid
-        minChildWidth="200px"
-        spacing={2}
-        maxW={'100%'}
-        padding={2}
-        my={4}
-      >
-        {poolItemsLoading ? (
-          [...Array(3)].map((_, index) => (
-            <Skeleton key={index} height={'150px'} width={'250px'} />
-          ))
-        ) : poolItems?.itemsInPool?.length > 0 ? (
-          poolItems?.itemsInPool?.map((poolItem) => (
-            <RentedNFT key={poolItem.NFTID} nftId={poolItem.NFTID} />
-          ))
-        ) : (
-          <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
-            0 items found
-          </Text>
-        )}
-      </SimpleGrid>
-    </Container>
+        <AccordionItem>
+          <AccordionButton
+            _hover={{
+              bg: '#FBAA0B',
+              transition: 'background-color 0.2s',
+              color: 'white'
+            }}
+            padding={10}
+          >
+            <Box flex="1" textAlign="left">
+              <Heading fontSize={40} fontFamily={'Bayon'}>
+                Your Items in Pools
+              </Heading>
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel paddingLeft={10} pb={4}>
+            <Text fontSize={25} fontFamily={'Big Shoulders Text'}>
+              These are the items you've placed in pools, available for others
+              to rent
+            </Text>
+            <SimpleGrid
+              minChildWidth="200px"
+              spacing={2}
+              maxW={'100%'}
+              padding={2}
+              my={4}
+            >
+              {poolItemsLoading ? (
+                [...Array(3)].map((_, index) => (
+                  <Skeleton key={index} height={'150px'} width={'250px'} />
+                ))
+              ) : poolItems?.itemsInPool?.length > 0 ? (
+                poolItems?.itemsInPool?.map((poolItem) => (
+                  <RentedNFT key={poolItem.NFTID} nftId={poolItem.NFTID} />
+                ))
+              ) : (
+                <Text fontSize={24} fontFamily={'Big Shoulders Text'}>
+                  0 items found
+                </Text>
+              )}
+            </SimpleGrid>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Container >
   );
 }
