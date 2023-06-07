0a65f452b8cdde1570d20c1959796862fbd6e228
vinichagastrexx <vinicius@trexx.com.br>
2023-06-07 15:23:56 -0300

    fix: remove console.log

--- chainlinkFunctionsCode/calculateMarketplaceVolumeFactor.js
+++ chainlinkFunctionsCode/calculateMarketplaceVolumeFactor.js
@@ -1,11 +1,8 @@
 
-const resourceId = "MUMBAI2.TRANSACTIONS"
-const transactionVolumeQuery = "SELECT COUNT(*) \n" +
-  "FROM MUMBAI2.TRANSACTIONS \n" +
-  "WHERE MUMBAI2.TRANSACTIONS.TO_ADDRESS = '0x7A810cB14b16Fb68B6c64766865B5090d80a8Ff0' \n" +
-  "AND MUMBAI2.TRANSACTIONS.VALUE_ > '0' \n" +
-  "AND MUMBAI2.TRANSACTIONS.TIME_STAMP >= NOW() - INTERVAL 30 DAY \n";
-
+const resourceId = "TREXXGG.RENTS"
+const transactionVolumeQuery = "SELECT COUNT(*)\n" +
+  "FROM TREXXGG.RENTS\n" +
+  "AND TREXXGG.RENTS.INITDATE >= NOW() - INTERVAL 1 DAY \n";
 
 const response = await Functions.makeHttpRequest({
   url: "https://hackathon.spaceandtime.dev/v1/sql/dql",
@@ -24,7 +21,7 @@ console.log("Full response from SxT API:", response)
 console.log("Value we'll send on-chain:", parseInt(arrayResponse[0]));
 
 const transactionVolume = arrayResponse[0];
-const referenceVolume = 1000; // 100 transactions per day for the last 10 days
+const referenceVolume = 100; // 100 transactions per day for the last day
 
 //must be a number between 0 and 100;
 let volumeRatio = transactionVolume / referenceVolume;
