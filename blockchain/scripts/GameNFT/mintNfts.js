88915b01176e036afad44729905cbf6b502b9c7e
vinichagastrexx <vinicius@trexx.com.br>
2023-06-09 15:18:32 -0300

    feat: adjust scripts

---
+++ blockchain/scripts/GameNFT/mintNfts.js
@@ -0,0 +1,20 @@
+const env = require("../../config/env")
+const { ethers } = require("hardhat")
+
+async function main() {
+  const [owner] = await ethers.getSigners()
+  const GameNFT = await ethers.getContractFactory("GameNFT")
+  const gameNFT = await GameNFT.attach(env.nftContractAddress)
+  await gameNFT.connect(owner)
+  for(let i = 0; i < 5; i++) {
+    const tx = await gameNFT.mintTo(env.walletAddress, '')
+    console.log("NFT minted:", tx)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
