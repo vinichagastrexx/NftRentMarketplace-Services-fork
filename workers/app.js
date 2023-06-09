feb3845f91826ed0ae1c854110e1cb77592a8af5
vinichagastrexx <vinicius@trexx.com.br>
2023-06-09 12:24:20 -0300

    refactor: update worker console message

--- workers/app.js
+++ workers/app.js
@@ -3,7 +3,7 @@ const nftRentMarketplaceWorker = new NFTRentMarketplaceEventWorker();
 
 try {
   nftRentMarketplaceWorker.init();
-  console.log('app listening')
+  console.log(`app listening to ${process.env.NFT_RENT_MARKETPLACE_CONTRACT} contract events`)
 } catch (e) {
   console.error(e)
 }
