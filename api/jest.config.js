7aaa852bb70336aa69e14b49caf3b2d03161d281
vinichagastrexx <vinicius@trexx.com.br>
2023-07-21 10:39:24 -0300

    update jest.config.js

--- api/jest.config.js
+++ api/jest.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testPathIgnorePatterns: ['<rootDir>/build/'],
+  setupFilesAfterEnv: ['./tests/setupTests.ts'],
 };
